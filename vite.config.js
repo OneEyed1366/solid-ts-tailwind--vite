cb0734a9e8cc01890752e11889924a7f6e4c61a5
OneEyed1366 <psevdoproger@gmail.com>
2022-09-26 09:48:13 +0300

    refactor

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,14 @@ import { defineConfig } from "vite";
 import solidPlugin from "vite-plugin-solid";
 import Icons from "unplugin-icons/vite";
 import AutoImport from "unplugin-auto-import/vite";
+import path from "path";
 
 export default defineConfig({
+  resolve: {
+    alias: {
+      src: path.resolve(__dirname, "./src"),
+    },
+  },
   plugins: [
     solidPlugin(),
     AutoImport({
